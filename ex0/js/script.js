57d467d6bde3e1ff34c69dcfed31cfe8ae182b39
DmitrySulimov <sulimovdmitryvnua@gmail.com>
2017-11-27 17:28:57 +0200

    added missed js

---
+++ ex0/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: true,
+        // dots: true,
+    });
+});
